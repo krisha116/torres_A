43c40a5adc822e4c69e849e81cc37107c0df98e2
krisha <krishatorres116@gmail.com>
2026-02-10 17:35:09 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>KRISHA-ANN TORRES</Text>
+      <Text style={{ fontSize: 10, marginTop: 10 }}>Bachelor of Science in Information Technology - students</Text>
+    </View>
+  )
+}
+
+export default App

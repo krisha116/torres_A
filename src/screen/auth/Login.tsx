790d95146510be4cfa56655683e0ce7ccc3eee1c
krisha <krishatorres116@gmail.com>
2026-05-07 18:45:23 +0800

    feat: partial google sign in

--- src/screen/auth/Login.tsx
+++ src/screen/auth/Login.tsx
@@ -1,259 +1,3 @@
-// import { useState, useEffect } from 'react';
-// import { Image, Alert, Text, TouchableOpacity, View } from 'react-native';
-// import { useNavigation } from '@react-navigation/native';
-// import { useDispatch, useSelector } from 'react-redux';
-
-// import CustomButton from '../../components/CustomButton';
-// import CustomTextInput from '../../components/CustomTextInput';
-// import { IMG, ROUTES } from '../../utils';
-// import { authLogin } from '../../app/actions';
-
-// const Login = () => {
-//     const [username, setUsername] = useState('');
-//     const [password, setPassword] = useState('');
-
-//     const navigation = useNavigation();
-//     const dispatch = useDispatch();
-//     const auth = useSelector(state => state.auth);
-
-//     useEffect(() => {
-//         if (!auth.isLoading && auth.isError && auth.error) {
-//             Alert.alert('Login failed', auth.error);
-//         }
-//     }, [auth.isLoading, auth.isError, auth.error]);
-
-//     return (
-//         <View
-//         style={{
-//             flex: 1,
-//             padding: 20,
-//             alignItems: 'center',
-//             justifyContent: 'center',
-//         }}
-//         >
-//         <Image
-//             source={IMG.LOGO}
-//             style={{ width: 240, height: 80, marginBottom: 40 }}
-//             resizeMode="contain"
-//         />
-
-//         <View style={{ width: '100%' }}>
-//             <CustomTextInput
-//             label={'Username'}
-//             placeholder={'Enter Username'}
-//             value={username}
-//             onChangeText={val => setUsername(val)}
-//             containerStyle={{
-//                 padding: 5,
-//             }}
-//             textStyle={{
-//                 borderRadius: 10,
-//                 color: 'black',
-//                 marginLeft: 10,
-//                 fontWeight: 'bold',
-//             }}
-//             />
-//             <CustomTextInput
-//             label={'Password'}
-//             placeholder={'Enter Password'}
-//             value={password}
-//             onChangeText={val => setPassword(val)}
-//             containerStyle={{
-//                 padding: 5,
-//             }}
-//             textStyle={{
-//                 borderRadius: 10,
-//                 color: 'black',
-//                 marginLeft: 10,
-//                 fontWeight: 'bold',
-//             }}
-//             />
-//         </View>
-
-//         <CustomButton
-//             label={'LOGIN'}
-//             containerStyle={{
-//             backgroundColor: '#1E3A8A',
-//             borderRadius: 10,
-//             marginVertical: 20,
-//             width: '85%',
-//             }}
-//             textStyle={{
-//             color: 'white',
-//             fontWeight: 'bold',
-//             }}
-//             onPress={() => {
-//             if (username === '' || password === '') {
-//                 Alert.alert(
-//                 'Invalid Credentials',
-//                 'Please enter valid userame and password',
-//                 );
-
-//                 return;
-//             }
-
-//             dispatch(
-//                 authLogin({
-//                 username,
-//                 password,
-//                 }),
-//             );
-//             }}
-//         />
-
-//         <View
-//             style={{
-//             flexDirection: 'row',
-//             alignItems: 'center',
-//             justifyContent: 'center',
-//             }}
-//         >
-//             <Text>Create an account?</Text>
-//             <TouchableOpacity onPress={() => navigation.navigate(ROUTES.REGISTER)}>
-//             <Text style={{ color: 'green', marginLeft: 5, fontWeight: 'bold' }}>
-//                 Register
-//             </Text>
-//             </TouchableOpacity>
-//         </View>
-//         </View>
-//     );
-// };
-
-// export default Login;
-
-// import { useEffect, useState } from 'react'
-// import { View,
-//      Text, 
-//      TouchableOpacity, 
-//      Alert, 
-//      StyleSheet, 
-//      Image} from 'react-native'
-
-
-// import CustomTextInput from '../../components/CustomTextInput';
-// import CustomButton from '../../components/CustomButton';
-// import { useNavigation } from '@react-navigation/native';
-// import { ROUTES } from '../../utils';
-// import {userLogin} from '../../app/api/auth';
-// import { useDispatch, useSelector } from 'react-redux';
-// import {authLogin} from '../../app/reducers/auth';
-// import IMG from '../../utils/images';
-
-
-// const Login = () => {
-
-// const [username, setUsername] = useState('');
-// const [password, setPassword] = useState('');
-
-
-// const navigations = useNavigation();
-// const dispatch = useDispatch();
-// const auth = useSelector(state => state.auth);
-
-
-// useEffect(() => {
-//     if (!auth.isLoading && auth.isError && auth.error){
-//         Alert.alert('Login failed', auth.error);
-//     }
-// }, 
-//     [auth.isLoading, auth.isError, auth.error]);
-
-
-//   return (
-//     <View style={{
-//      flex: 1,
-//      padding: 20,
-//      alignItems: 'center',
-//      justifyContent: 'center' }}>
-     
-//       {/* <Text>Email Address: {emailAdd}</Text>
-//       <Text>Password: {password}</Text> */}
-
-//       <Image
-//             source={IMG.LOGO2}
-//             style={{ width: 240, height: 80, marginBottom: 40 }}
-//             resizeMode="contain"
-//         />
-//      <View style={{
-//       width: '100%',
-//       }}>
-
-    
-
-//       <CustomTextInput label={'Username'} 
-//       placeholder={'Enter Username'} 
-//       value={(val) => setUsername(val)}
-//       containerStyle={{ 
-//         padding: 10,
-//       }}
-//       textStyle={{ borderRadius: 10, color: 'black', marginLeft: 15}} />
-
-//       <CustomTextInput label={'Password'}
-//        placeholder={'Enter Password'}
-//        value={(val) => setPassword(val)}
-//        containerStyle={{ 
-//         padding: 10,
-        
-//       }}
-//       textStyle={{ borderRadius: 10, 
-//         color: 'red', 
-//         marginLeft: 15}}
-//        />
-//      </View>
-
-//       <CustomButton label={'LOG IN'} 
-//       containerStyle={{   
-//       borderWidth:1, 
-//       borderRadius: 10,
-//       marginVertical: 20,
-//       backgroundColor: 'blue',
-//       width: '85%',
-      
-//       }} 
-//       textsStyle={{ color: 'white', 
-//         fontWeight: 'bold'}}
-
-//       onPress={() => {
-//         if (username === '' || password === '') {
-//           Alert.alert(
-//             'Invalid Credentials', 
-//             'Please enter valid username and password');
-
-//             return;
-//         }
-
-//         dispatch(authLogin({
-//           username: username,
-//           password: password,
-//         }));
-
-//       }}
-//        />
-
-//        <View style={{
-//         flexDirection: 'row',
-//         justifyContent: 'center',
-//         alignItems: 'center',}}>
-            
-            
-//         <Text> Create an Account </Text>
-        
-//         <TouchableOpacity onPress={() => navigations.navigate(ROUTES.REGISTER)}>
-            
-//           <Text style={{color: 'red',
-//              fontWeight: 'bold',
-//              marginLeft: 10,
-//              }}> Register </Text>   
-
-//         </TouchableOpacity>
-
-//       </View>
-//     </View>
-//   )
-// }
-
-// export default Login
-
 
 import React, { useEffect, useState } from 'react'
 import { 
@@ -274,6 +18,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import CustomTextInput from '../../components/CustomTextInput';
 import CustomButton from '../../components/CustomButton';
 import { authLogin } from '../../app/reducers/auth';
+import { GoogleSigninButton } from '@react-native-google-signin/google-signin';
+import { _signInwithGoogle } from '../../utils/firebase';
 
 const FOX_THEME = {
   orange: '#53e622',
@@ -316,6 +62,26 @@ const Login: React.FC = () => {
       password,
     }));
   };
+
+      <GoogleSigninButton
+        //  style={{ width: 212, height: 48 }}
+         size={GoogleSigninButton.Size.Wide}
+         color={GoogleSigninButton.Color.Dark}
+         onPress={async () => {
+          
+          await _signInwithGoogle().then((result) =>{
+            console.log(result);
+          })
+          .catch((err) => {
+            Alert.alert('Error', `${err.message}`);
+          }).finally(() => {
+            Alert.alert('Success', 'Google Sign-In successful');
+          });
+        
+        }}
+       />
+
+
 
   return (
     <View style={[
